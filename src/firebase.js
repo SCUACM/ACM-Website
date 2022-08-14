ed81848656d61ac813998069fd57a2ec5e419f96
Jordan Mosakowski <35510468+jordanmosakowski@users.noreply.github.com>
2022-08-13 20:21:05 -0700

    Firebase v9 fix

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-import 'firebase/storage';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import 'firebase/compat/storage';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDTCNXN1akZ6DEWKLGyOp2JZvAworux9jI",
