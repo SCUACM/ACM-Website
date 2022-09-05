bbd66432d7bc0fa7f2f5ee7bc0f2ef09cd560a9d
tmdwnsyang <95985696+tmdwnsyang@users.noreply.github.com>
2022-09-05 10:15:06 -0700

    Basic uploading functionality
    resume is added to the cloud storage via path resume/.

--- src/resume.js
+++ src/resume.js
@@ -1,72 +1,79 @@
-import {initializeApp} from 'https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js";
 // import 'firebase/compat/firestore';
 // import 'firebase/compat/auth';
-import {getStorage} from 'https://www.gstatic.com/firebasejs/9.9.0/firebase-storage.js';
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+} from "https://www.gstatic.com/firebasejs/9.9.0/firebase-storage.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDTCNXN1akZ6DEWKLGyOp2JZvAworux9jI",
-    authDomain: "scu-acm.firebaseapp.com",
-    projectId: "scu-acm",
-    storageBucket: "scu-acm.appspot.com",
-    messagingSenderId: "561382074280",
-    appId: "1:561382074280:web:e3e8ca43e1a5270b519f9d",
-    measurementId: "G-9ELQ4BE3XH"
-  };
+  apiKey: "AIzaSyDTCNXN1akZ6DEWKLGyOp2JZvAworux9jI",
+  authDomain: "scu-acm.firebaseapp.com",
+  projectId: "scu-acm",
+  storageBucket: "scu-acm.appspot.com",
+  messagingSenderId: "561382074280",
+  appId: "1:561382074280:web:e3e8ca43e1a5270b519f9d",
+  measurementId: "G-9ELQ4BE3XH",
+};
 
 const firebase = initializeApp(firebaseConfig);
 
 // Initialize Cloud Storage and get a ref to the service
 const storage = getStorage(firebase);
 
-let dropArea = document.getElementById('dropArea');
+// Create a storage ref from storage service
+const storageRef = ref(storage);
 
-['dragenter', 'dragover', 'dragleave','drop'].forEach((event) => {
+let dropArea = document.getElementById("dropArea");
+let fileInput = document.getElementById("fileElement");
+
+fileInput.addEventListener("change", fileHandler, false);
+
+["dragenter", "dragover", "dragleave", "drop"].forEach((event) => {
   dropArea.addEventListener(event, preventDefaults, false);
 });
 
-function preventDefaults(e){
-    e.preventDefaults();
-    e.stopPropagation();
+function preventDefaults(e) {
+  e.preventDefaults();
+  e.stopPropagation();
 }
 
-['dragenter','dragover'].forEach((event) => {
-    dropArea.addEventListener(event, highlight, false)
-    });
-['dragleave', 'drop'].forEach((event) => 
-    dropArea.addEventListener(event, removeHighlight, false));
+["dragenter", "dragover"].forEach((event) => {
+  dropArea.addEventListener(event, highlight, false);
+});
+["dragleave", "drop"].forEach((event) =>
+  dropArea.addEventListener(event, removeHighlight, false)
+);
 
 function highlight(e) {
-    dropEvent.classList.add('highlight');
+  dropEvent.classList.add("highlight");
 }
 
-function removeHighlight(e){
-    dropEvent.classList.remove('highlight');
+function removeHighlight(e) {
+  dropEvent.classList.remove("highlight");
 }
 
-dropArea.addEventListener('drop', dropHandler, false);
+dropArea.addEventListener("drop", dropHandler, false);
 
-function dropHandler(e){
-    let data = e.dataTransfer;
-    let files = data.files;
-    
-    fileHandler(files);
+function dropHandler(e) {
+  let data = e.dataTransfer;
+  let files = data.files;
+
+  fileHandler(files);
 }
 
-function fileHandler(files){
-    // iterating through the files individually...
-    ([...files]).forEach(fileUpload);
+function fileHandler(e) {
+  // iterating through the files individually...
+  [...e.target.files].forEach(fileUpload);
 }
 
-function fileUpload(file){
-    // firebase stuff goes here
-    let url = 'gs://scu-acm.appspot.com/resumes';
-    let formData = new FormData();
-    
-    formData.append('file', file);
+function fileUpload(file) {
+  const resumeStorageRef = ref(storage, "resumes/resume_name.pdf");
 
-    fetch(url, {
-        method: 'POST', body: formData
-    }).then(() => { console.log('success.'); })
-    .catch(console.log('error'));
+  uploadBytes(resumeStorageRef, file).then((snapshot) =>{
+    console.log('uploaded resume!');
+  });
+
+  console.log("test");
 }
-
