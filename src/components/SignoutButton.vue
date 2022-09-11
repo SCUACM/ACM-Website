c51ce458c26df4e9ba75283fee5bc30d0a4b4c88
kav-vinod <68215358+kav-vinod@users.noreply.github.com>
2022-09-10 21:57:28 -0700

    Sign out

---
+++ src/components/SignoutButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div @click="SignOut()">
+    <v-btn class=blue white--text> Sign In </v-btn>
+  </div>
+</template>
+
+
+methods: {
+  SignOut() {
+  import { getAuth, signOut } from "firebase/auth";
+
+  const auth = getAuth();
+  signOut(auth).then(() => {
+  // Sign-out successful.
+  }).catch((error) => {
+  // An error happened.
+  });
+}
+}
