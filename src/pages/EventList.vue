b0c91e493c66d29a13c804b7ed881194754ffafe
Trashman247 <rnwong@scu.edu>
2023-05-29 21:04:56 -0700

    Create CalendarComponent and register Calendar page
    The CalendarComponent is a copy of the Calendar page without the Navbar and Footer and the v-app wrapper.

--- src/pages/EventList.vue
+++ src/pages/EventList.vue
@@ -3,20 +3,20 @@
     <Navbar />
     <v-container style="margin-top: 100px; max-width: 1000px;">
       <ul style="list-style: none; display: flex; justify-content: right; gap: 0.5rem; font-size: 1.25rem">
-        <li @click="view = 0" style="cursor: pointer" :style="[view == 0 ? {'color': '#0099ff'} : {'color': 'black'}]">
+        <li @click="view=0" style="cursor: pointer" :style="[view == 0 ? {'color': '#0099ff'} : {'color': 'black'}]">
           List
         </li>
         /
-        <li @click="view = 1" style="cursor: pointer" :style="[view == 1 ? {'color': '#0099ff'} : {'color': 'black'}]">
+        <li @click="view=1" style="cursor: pointer" :style="[view == 1 ? {'color': '#0099ff'} : {'color': 'black'}]">
           Grid
         </li>
         /
-        <li @click="view = 2" style="cursor: pointer" :style="[view == 2 ? {'color': '#0099ff'} : {'color': 'black'}]">
+        <li @click="view=2" style="cursor: pointer" :style="[view == 2 ? {'color': '#0099ff'} : {'color': 'black'}]">
           Calendar
         </li>
       </ul>
     </v-container>
-    <v-container style="max-width: 1000px">
+    <v-container v-if="view==0 || view==1" style="max-width: 1000px">
       <div class="events-title" v-if="upcoming.length">
         Upcoming Events
       </div>
@@ -32,6 +32,9 @@
         <EventCardGrid v-for="event of this.past" :event="event" :key="event.id" :big="false"/>
       </div>
     </v-container>
+
+    <CalendarComponent v-show="view==2"/>
+
     <Footer />
   </v-app>
 </template>
@@ -41,6 +44,7 @@ import "../assets/scss/board-media.scss";
 
 import EventCard from "@/components/EventCard.vue";
 import EventCardGrid from "@/components/EventCardGrid.vue";
+import CalendarComponent from "@/components/CalendarComponent.vue";
 import Navbar from "@/layout/Navbar.vue";
 import Footer from "@/layout/Footer.vue";
 
@@ -55,6 +59,7 @@ export default {
     Navbar,
     EventCard,
     EventCardGrid,
+    CalendarComponent,
     Footer,
   },
 
