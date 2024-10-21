69d0c2a1a5568f030fa5f326e35cdac3374084f3
RosalieWessels <rosalie.wessels@yahoo.com>
2024-10-20 23:11:37 -0700

    Updated ACM-W Board & deleted AIC

--- src/components/BoardList.vue
+++ src/components/BoardList.vue
@@ -6,8 +6,6 @@
 
     <BoardListSection title="BroncoSec" :memberList="broncoSecBoard" />
 
-    <BoardListSection title="AI Collaborate" :memberList="aiColabBoard" />
-
     <BoardListSection title="ACM-G" :memberList="acmGBoard" />
 
     <BoardListSection title="Competitive Programming" :memberList="competitveProgrammingBoard" />
@@ -24,7 +22,7 @@ import JakeEsperson from "../assets/board/main/Jake_Esperson.jpeg";
 import SamhitaRachapudi from "../assets/board/main/Samhita_Rachapudi.jpeg";
 import AnishKatragadda from "../assets/board/main/Anish_Katragadda.jpeg";
 import JalenPaige from "../assets/board/main/Jalen_Paige.jpeg";
-import RosalieWessels from "../assets/board/main/Rosalie_Wessels.jpeg";
+import RosalieWessels from "../assets/board/main/Rosalie_Wessels.jpg";
 import GriffinJones from "../assets/board/main/Griffin_Jones.jpeg";
 import ChrisShobe from "../assets/board/main/Chris_Shobe.jpeg";
 import IshikaGupta from "../assets/board/main/Ishika_Gupta.jpeg";
@@ -42,30 +40,18 @@ import VarunMangla from "../assets/board/broncosec/Varun_Mangla.jpg";
 import AdrianZhang from "../assets/board/broncosec/Adrian_Zhang.jpg";
 import JasonWu from "../assets/board/broncosec/Jason_Wu.jpg";
 
-import AnayaGoradia from "../assets/board/ACM-W/Anaya_Goradia.jpg";
-import LeilaErhili from "../assets/board/ACM-W/Leila_Erhili.jpg";
-import IsabellaAguilar from "../assets/board/ACM-W/Isabella_Aguilar.jpg";
-import GwendolynPatwardhan from "../assets/board/ACM-W/Gwendolyn_Patwardhan.jpg";
-import MeaganVu from "../assets/board/ACM-W/Meagan_Vu.jpg";
-import AvaSimmons from "../assets/board/ACM-W/Ava_Simmons.jpg";
-import IreneChang from "../assets/board/ACM-W/Irene_Chang.jpg";
-import PragnyaVijayan from "../assets/board/ACM-W/Pragnya_Vijayan.png";
-import EmmaBoctor from "../assets/board/ACM-W/Emma_Boctor.jpg";
-import TruptiJoshiSatish from "../assets/board/ACM-W/Trupti_Joshi_Satish.jpg";
-import MandyLin from "../assets/board/ACM-W/Mandy_Lin.jpg";
-import MaddieFollosco from "../assets/board/ACM-W/Maddie_Follosco.jpg";
-import BeatriceHackman from "../assets/board/ACM-W/Beatrice_Hackman.jpg";
-import MadiNguyen from "../assets/board/ACM-W/Madi_Nguyen.jpg";
-import GwynethAnawalt from "../assets/board/ACM-W/Gwyneth_Anawalt.jpg";
-import ChristinaDai from "../assets/board/ACM-W/Christina_Dai.jpg";
-import SanjnaSood from "../assets/board/ACM-W/Sanjna_Sood.jpg";
+import CatherineHung from "../assets/board/ACM-W/Catherine_Hung.jpeg";
+import LeilaErhili from "../assets/board/ACM-W/Leila_Erhili.jpeg";
+import AnnaAldrin from "../assets/board/ACM-W/Anna_Aldrin.jpeg";
+import AryaDeshpande from "../assets/board/ACM-W/Arya_Deshpande.jpeg";
+import SamiaAhmer from "../assets/board/ACM-W/Samia_Ahmer.jpeg";
+import IreneChang from "../assets/board/ACM-W/Irene_Chang.jpeg";
+import MaddieFollosco from "../assets/board/ACM-W/Maddie_Follosco.jpeg";
+import SanjnaSood from "../assets/board/ACM-W/Sanjna_Sood.jpeg";
 import PrakritiPatnaik from "../assets/board/ACM-W/Prakriti_Patnaik.jpg";
-
-import MichaelIwashima from "../assets/board/aicolab/Michael_Iwashima.jpg"
-import SayanJain from "../assets/board/aicolab/Sayan_Jain.jpg"
-import Aiana from "../assets/board/aicolab/Aiana.jpg"
-import Kim from "../assets/board/aicolab/Kim.jpg"
-import KayaChun from "../assets/board/main/Kaya_Chun.jpg";
+import TrishaGanesh from "../assets/board/ACM-W/Trisha_Ganesh.jpeg";
+import SattvikaBhatt from "../assets/board/ACM-W/Sattvika_Bhatt.jpeg";
+import AnanyaPagadala from "../assets/board/ACM-W/Ananya_Pagadala.jpeg";
 
 import JordanRandleman from "../assets/board/ACM-G/Jordan_Randleman.jpg"
 import LandisFusato from "../assets/board/ACM-G/Landis_Fusato.jpg"
@@ -181,128 +167,86 @@ export default {
 
       acmWBoard: [
         {
-          src: AnayaGoradia,
-          name: "Anaya Goradia",
-          role: "President",
-          bio: 
-            "Hi! My name is Anaya and I am a junior majoring in Computer Science from San Ramon, California. I first realized that I liked computer science because of the thrill that you get when you finally solve a coding or math problem. Every problem is a puzzle with a new solution! I decided to major in Computer Science because it has so many applications in today's world and the possibilities are endless. Other than coding and math I love to paint or do anything art related. For fun I love to bake with my family and I'm always ready for a boba run. I’m excited to be a part of ACM-W and getting to know everyone!"
-        },
-        {
           src: LeilaErhili,
           name: "Leila Erhili",
-          role: "Vice President",
+          role: "President",
           bio:
-            "Hello! My name is Leila and I am a junior studying Computer Science and Engineering here at SCU. I absolutely love computer science, so I am a huge advocate for getting more women the opportunity to discover how fun and satisfying programming can be. I actually did not begin my college journey studying Computer Science and Engineering. My freshman year, I took a C++ class out of curiosity, and immediately knew that I had found what I wanted to study. I then switched majors and have never looked back. Other pastimes that I enjoy include moving my body (ie. gym, hiking, etc,) , thrifting, writing, and of course, spending time with the people closest to me. I am so excited to be part of such an amazing community of amazing women and can’t wait to get to know everyone! "
-        },
-        {
-          src: IsabellaAguilar,
-          name: "Isabella Aguilar",
-          role: "Secretary ",
-          bio:
-            "Hi! My name is Isabella Aguilar and I’m a current junior studying Computer Science from Santa Clara. Prior to attending Santa Clara, I had minimal experience coding. However, after my first computer science course freshman year I knew it was what I wanted my career to be! I love being creative and having the ability to create something new! Outside of school, I love to travel and be outside. Whether it be a new coffee shop or go to a new local beach! I am really excited to be a part of ACM-W and look forward to working with a supportive community of women!"
-        },
-        {
-          src: GwendolynPatwardhan,
-          name: "Gwendolyn Patwardhan",
-          role: "Treasurer",
-          bio:
-            "Hello! I’m Gwendolyn Patwardhan, a junior Computer Science and Engineering major and looking into art and math minors. I have always loved problem solving and creation and I found computer science to be the perfect fusion of the two. Currently, I am most interested in UX/UI design & research and game development–I currently work with SCU’s game development team in the Imaginarium. In my free time I love drawing, reading, and playing games with friends. I look forward to having a great year with you all!"
-        },
-        {
-          src: MeaganVu,
-          name: "Meagan Vu",
-          role: "Director of Events",
-          bio:
-            "Hey! My name’s Meagan and I’m a junior studying Computer Science Engineering. Originally I came to SCU as a business major, but as soon as I took my first coding course here, I knew I wanted to code for the rest of my life (or until retirement)! Whenever I start a project, my mind goes on autopilot and the process just flows through me and the satisfaction of actually making a function program is so rewarding. Other than coding, my personality trait is being a gym rat. Excited to be apart of this program and come say hi to me if you see me in Malley ;)"
-        },
-        {
-          src: AvaSimmons,
-          name: "Ava Simmons",
-          role: "Event Coordinator",
-          bio:
-            ""
-        },
-        {
-          src: IreneChang,
-          name: "Irene Chang",
-          role: "Co-Event Coordinator",
-          bio:
-            ""
-        },
-        {
-          src: PragnyaVijayan,
-          name: "Pragnya Vijayan",
-          role: "Co-Event Coordinator",
-          bio: 
-            ""
-        },
-        {
-          src: EmmaBoctor,
-          name: "Emma Boctor",
-          role: "Hackathon Coordinator",
-          bio:
-            "Hi! My name is Emma Boctor, and I am a sophomore studying Computer Science & Engineering from Seattle, Washington. I first realized that I liked computer science when I worked at a STEM school teaching kids how to code. Seeing the creativity through the kids made me realize that you can really build whatever you set your mind to, and that made me excited to pursue a career in CSE! A couple things you may not know about me is that I am Egyptian and Australian and whether I like to admit it or not, I am a Red Bull addict. Outside of school I love to play basketball, take photos on my film camera, edit videos, and spend time with friends and family. I look forward to being a part of ACM-W and getting to know everyone!"
-        },
-        {
-          src: TruptiJoshiSatish,
-          name: "Trupti Joshi Satish",
-          role: "Hackathon Coordinator",
-          bio:
-            "Hello, everyone! I'm Trupti Joshi, a second-year graduate student majoring in Computer Science from Santa Clara, CA. I've always been driven by curiosity and a strong work ethic, constantly seeking to uncover the endless possibilities within computer science. I'm particularly passionate about AI, ML, and distributed systems, as they hold the power to revolutionize technology and make our lives better. Currently, I'm gaining valuable experience as a Web Application Developer for the University Website, while also interning at Ciocca Center as a Graphics Intern and at Miller Center as a Data Engineer Intern. When I'm not immersed in academics, you'll find me singing, seeking thrilling adventures, and exploring new destinations. I'm beyond excited to be a part of ACM-W and can't wait to connect with everyone!"
-        },
-        {
-          src: MandyLin,
-          name: "Mandy Lin",
-          role: "Director of Media",
-          bio: 
-            "Hi! My name is Mandy and I'm a sophomore studying Computer Science and Graphic Design from San Jose, California. I first got started in computer science in high school and have enjoyed its ability to problem solve and its limitless applications ever since. Currently, I'm interested in web development and design, but I’m still exploring more in the field. Outside of computer science, I enjoy dancing, crocheting, and making things. I’m super excited to be a part of ACM-W and to meet new people!"
+            "Hello! My name is Leila and I am a senior studying Computer Science and Engineering here at SCU. I absolutely love computer science, so I am a huge advocate for getting more women the opportunity to discover how fun and satisfying programming can be. I actually did not begin my college journey studying Computer Science and Engineering. My freshman year, I took a C++ class out of curiosity, and immediately knew that I had found what I wanted to study. I then switched majors and have never looked back. Other pastimes that I enjoy include moving my body (ie. gym, hiking, etc,) , thrifting, writing, and of course, spending time with the people closest to me. I am so excited to be part of such an amazing community of amazing women and can’t wait to get to know everyone! "
         },
         {
           src: MaddieFollosco,
           name: "Maddie Follosco",
-          role: "Director of Public Relations",
+          role: "Vice President",
           bio: 
             "Hey! My name is Maddie Follosco and I am a Computer Science and Engineering major from Santa Clarita, California. In high school I thought I wanted to go into the medical field because I wanted to have a lasting impact on people's lives. However, after job shadowing engineers at a local medical devices startup, I realized I could still have that altruistic impact as an engineer by designing cool things for people. And so naturally my love for math and logic led me here and I am loving it so far. I am interested in exploring all the fields of computer science especially machine learning and game design. In my free time, I am an avid Dodgers fan, I am obsessed with pop music and R&B, and I love spending quality time with friends and family. I look forward to an amazing year with the gals at ACM-W!"
         },
         {
-          src: BeatriceHackman,
-          name: "Beatrice Hackman",
-          role: "Industry Liaison",
-          bio: 
-            "Hello! I am Beatrice Hackman and I am currently a junior Web Design major from the Bay Area. In my free time I love to spend time with family and friends, take photos, hike, and binge true crime shows. Whether it was graphics or birthday cards, I always loved designing things, and so when I first learned to code, I knew that I would love web development. I am so excited to be a part of ACM-W and meet everyone!"
-        },
-        {
-          src: MadiNguyen,
-          name: "Madi Nguyen",
-          role: "Industry Liaison",
-          bio: 
-            "Hi, my name is Madison Nguyen! I’m a junior from San Jose, California studying Web Design and Engineering. I learned about computer science in middle school from Girls Who Code and to be honest I initially hated it. However, I always admired my mom in her field, and in high school, I tried again. I was pleasantly surprised by my new love for design, helping classmates debug their code, and assisting my AP Computer Science teachers! This year I am working at the SCU Alumni Association as their Web Specialist so you are always welcome to come in and say hello! In my free time, I enjoy making eccentric Spotify playlists, trying out good food and boba spots, and making new memories with my friends. I’m excited to be ACM-W’s new secretary and I cannot wait to meet you all! :)"
-        },
-        {
-          src: GwynethAnawalt,
-          name: "Gwyneth Anawalt",
-          role: "Outreach Coordinator",
-          bio: 
-            ""
-        },
-        {
-          src: ChristinaDai,
-          name: "Christina Dai",
-          role: "Webmaster",
-          bio: 
+          src: CatherineHung,
+          name: "Catherine Hung",
+          role: "Treasurer",
+          bio:
             ""
         },
         {
           src: SanjnaSood,
           name: "Sanjna Sood",
-          role: "Underclassman Representative",
+          role: "Secretary",
           bio: 
+            ""
+        },
+        {
+          src: IreneChang,
+          name: "Irene Chang",
+          role: "Director of Events",
+          bio:
+            ""
+        },
+        {
+          src: AnnaAldrin,
+          name: "Anna Aldrin",
+          role: "Event Coordinator",
+          bio:
+            ""
+        },
+        {
+          src: AryaDeshpande,
+          name: "Arya Deshpande",
+          role: "Event Coordinator",
+          bio:
+            ""
+        },
+        {
+          src: SamiaAhmer,
+          name: "Samia Ahmer",
+          role: "Media and Public Relations",
+          bio:
             ""
         },
         {
           src: PrakritiPatnaik,
           name: "Prakriti Patnaik",
-          role: "Underclassman Representative",
+          role: "Webmaster",
+          bio: 
+            ""
+        },
+        {
+          src: TrishaGanesh,
+          name: "Trisha Ganesh",
+          role: "Industry Liaison",
+          bio: 
+            ""
+        },
+        {
+          src: SattvikaBhatt,
+          name: "Sattvika Bhatt",
+          role: "Hackathon Coordinator",
+          bio: 
+            ""
+        },
+        {
+          src: AnanyaPagadala,
+          name: "Ananya Pagadala",
+          role: "Conference/Career Opportunity Strategist",
           bio: 
             ""
         },
@@ -375,44 +319,6 @@ export default {
           name: "Jason Wu",
           role: "Underclassmen Rep",
           bio: ""
-        },
-      ],
-
-      aiColabBoard: [
-        {
-          src: MichaelIwashima,
-          name: "Michael Iwashima",
-          role: "President",
-          bio:
-            ""
-        },
-        {
-          src: SayanJain,
-          name: "Sayan Jain",
-          role: "",
-          bio:
-            ""
-        },
-        {
-          src: Aiana,
-          name: "Aiana",
-          role: "",
-          bio:
-            ""
-        },
-        {
-          src: KayaChun,
-          name: "Kaya Chun",
-          role: "",
-          bio:
-            "Hi! I’m Kaya Chun, a junior biomedical engineering student from all over - Georgia, Maryland, Virginia, and California! My interest in programming comes from the applications to the medical field and the ways that AI has continuously advanced clinical care. I started with object oriented programming and have furthered my interest with self research and projects. Apart from coding, I love working hands-on in labs and with my professors in their research. Beyond my academics, I love dance, drawing, and all the arts. I love spending time with friends exploring and experiencing new things together. Last but not least, I love animals and I love learning about people’s experiences with them. I’m so excited to see where my journey takes me and want to experience it with all of you so please don’t hesitate to say hi or join me on my journey!"
-        },
-        {
-          src: Kim,
-          name: "Kim",
-          role: "",
-          bio:
-            ""
         },
       ],
 
